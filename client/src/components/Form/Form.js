153e3a6f5fddead6536563401dd5a40788f0ac87
Sam Levy <slevy239@gmail.com>
2020-03-06 09:21:32 -0500

    added state

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: 200,
-    },
-  },
-}));
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     '& > *': {
+//       margin: theme.spacing(1),
+//       width: 200,
+//     },
+//   },
+// }));
 
-export default function BasicTextFields() {
-  const classes = useStyles();
+// export default function BasicTextFields() {
+//   const classes = useStyles();
+class Form extends Component {
+  // Setting the component's initial state
+  state = {
+    data: ''
+  };
+  handleInputChange = event => {
+    let value = event.target.value;
+    const name = event.target.name;
 
-  return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <Button variant="contained"  color="primary">Submit</Button>
-    </form>
-  );
+    this.setState({
+      [name]: value
+    })
+  }
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      data: ''
+    })
+    console.log('')
+
+  }
+  render() {
+
+    return (
+      <div>
+        <form noValidate autoComplete="off">
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" onChange={this.handleInputChange} value={this.state.data} 
+          />
+        </form>
+        <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>Submit</Button>
+      </div>
+    );
+  }
 }
+export default Form;
+
