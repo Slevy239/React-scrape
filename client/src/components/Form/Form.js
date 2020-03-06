c0d2a7ddbcf4e7e9b854deea2b132a14b1fed843
Sam Levy <slevy239@gmail.com>
2020-03-06 09:04:29 -0500

    form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-const pp='';
-class Form extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      
-    }
-  }
-  render() {
-    return (
-      <div>
-<textarea onChange={pp}></textarea>
-<button></button>
-      </div>
-    )
-  }
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}));
+
+export default function BasicTextFields() {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <Button variant="contained"  color="primary">Submit</Button>
+    </form>
+  );
 }
-export default Form
