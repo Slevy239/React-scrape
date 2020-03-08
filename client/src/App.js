27244a1fd5e018a20257884c502b7001cf9ac7b5
Sam Levy <slevy239@gmail.com>
2020-03-08 19:44:34 -0400

    links

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home'
 import './App.css';
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Router>
         <div>
           <Switch>
