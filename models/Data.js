b0fc3e8cd6123063c1c3f00b95e5f06f42305bd1
Sam Levy <slevy239@gmail.com>
2020-03-08 17:07:02 -0400

    model

--- models/Data.js
+++ models/Data.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
  teams: {required: true, type: Array},
-//  commence_time: {required: true },
-//  home_team: {required: true, type:String},
-//  sites: {required: true, type: String}
+ commence_time: {required: false },
+ home_team: {required: false, type:String},
+ sites: {required: false, type: String}
 });
 
 const Data = mongoose.model("Data", bookSchema);
