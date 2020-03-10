148ca8796cf9a4906cc2fe540e6cc787cd2ab137
Sam Levy <slevy239@gmail.com>
2020-03-09 21:22:06 -0400

    gits

--- client/src/pages/Gif.js
+++ client/src/pages/Gif.js
@@ -3,51 +3,56 @@ import axios from 'axios';
 import { Row, Col } from '../components/Grid'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia'
+// import CardMedia from '@material-ui/core/CardMedia'
 import EmptyList from '../components/EmptyList';
 import { List, ListItem } from "../components/List";
+import Image from 'react-bootstrap/Image'
 
 class Gif extends Component {
     state = {
         searchRes: [],
-        query: '',
+        query: "",
         results: []
     }
+
     componentDidMount() {
         this.searchApi();
     }
 
     displayRes = data => {
         this.setState({ results: data })
-        console.log(data)
+        // console.log(data)
     }
     searchApi = () => {
+        // const proxyurl = "https://cors-anywhere.herokuapp.com/";
         const key = 'tfZU03Q2B2QpE3Wn8OdWajo0JImv7arL';
         let url = 'https://api.giphy.com/v1/gifs/trending?api_key=' + key
         axios
             .get(url)
             .then(res => {
-                console.log(res.data);
-                this.displayRes(res.data)
+                // console.log(res.data.data[0].url);
+                this.displayRes(res.data.data)
             })
             .catch(err => console.log(err));
     }
     render() {
-        console.log(this.state)
+        console.log(this.state.results[1])
         return (
             <div>
                 <Row>
                     <Col size='md-12'>
                         <div id='gifs'>
-                            {(this.state.gifs && this.state.gifs.length > 0) ?
+                            {(this.state.results && this.state.results.length > 0) ?
                                 <List>
-                                    {this.state.gifs.map(result => {
+                                    {this.state.results.map(result => {
                                         return (
                                             <div id='item' key={result.id} className={result.key}>
                                                 <Card variant='outlined'>
                                                     <CardContent>
-                                                        <CardMedia
-                                                        image={this.state.gifs.url}></CardMedia>
+                                                        <div>
+                                                            {/* <img src={this.state.results.url}></img> */}
+                                                            <Image src={this.state.results[1].bitly_url}></Image>
+                                                        </div>
                                                     </CardContent>
                                                 </Card>
                                             </div>
