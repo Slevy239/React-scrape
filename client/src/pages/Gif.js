6bdbcb5f3f064b0104c6e16286a1ae56ebe68ac9
Sam Levy <slevy239@gmail.com>
2020-03-10 18:11:26 -0400

    working

--- client/src/pages/Gif.js
+++ client/src/pages/Gif.js
@@ -1,64 +1,66 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { Row, Col } from '../components/Grid'
+// import { Row, Col } from '../components/Grid'
 import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent';
-// import CardMedia from '@material-ui/core/CardMedia'
-import EmptyList from '../components/EmptyList';
+// import CardContent from '@material-ui/core/CardContent';
 import { List, ListItem } from "../components/List";
-import Image from 'react-bootstrap/Image'
-import TextField from '../components/TextField/TextField'
+import TextField from '@material-ui/core/TextField';
+// import Search from '../components/Search';
+import GifapiSearch from '../components/GifapiSearch/GifSearch';
+// import Trending from '../components/Trending';
+import Loader from '../components/Gifsearch/loader'
 
 
 class Gif extends Component {
-    constructor(props) {
-        super(props)
+    constructor(props){
+        super(props);
         this.state = {
-            gifs: { data: [] }
+            loader : false,
+            searchText : '',
+            gif : {},
         };
     }
 
-    componentDidMount() {
-        fetch(`http://api.giphy.com/v1/gifs/trending?&api_key=tfZU03Q2B2QpE3Wn8OdWajo0JImv7arL&limit=10`)
-            .then(res => res.json())
-            .catch(error => console.error('Error:', error))
-            .then((result) => {
-                this.setState({
-                    gifs: result,
-                });
-            })
+    searchGif(searchText) {
+        this.setState({
+            loader: true
+        });
+        this.getUrl(searchText, gif => { // callback(myJson)
+            const getDetails = {
+                image : gif.data.fixed_height_downsampled_url,
+                title :  gif.data.title,
+                gifUrl : gif.data.url
+            }
+            this.setState({
+                loader : false,
+                searchText: searchText,
+                gif : getDetails
+            });
+        })
     }
+
+    getUrl(searchText, callback) {
+        const url = `http://api.giphy.com/v1/gifs/random?&tag=${searchText}&api_key=aUhPqNQX1V9W5wSNJ9J0FOaQdvwgHH7O`
+        fetch(url)
+        .then(response => response.json())
+        .catch(error => console.error('Error:', error))
+        .then(myJson => callback(myJson))
+    }
+
     render() {
-        // console.log(this.state.results[1])
         return (
-            <div>
-                <Row>
-                    <TextField />
-                    <Col size='md-12'>
-                        <div id='gifs'>
-                            <List>
-                                {this.state.gifs.data.map(item => {
-                                    return (
-                                        <div id='item' key={item.id} className={item.key}>
-                                            <Card variant='outlined'>
-                                                <CardContent>
-                                                    <div>
-                                                        <img src={item.images.original.url} />
-                                                        {/* <Image src={this.state.results[1].url} /> */}
-                                                    </div>
-                                                </CardContent>
-                                            </Card>
-                                        </div>
-                                    )
-                                })}
-                            </List>
-                        </div>
-                    </Col>
-                </Row>
+            <div className="container">
+                <h1 className="inscApp">The GIF Search </h1>
+                <GifapiSearch
+                    onSearch={this.searchGif.bind(this)}
+                />
+                <Loader
+                    loader={this.state.loader}
+                    data={this.state.gif}
+                />
+                {/* <Trending /> */}
             </div>
-        )
+        );
     }
-
-
 }
+
 export default Gif
