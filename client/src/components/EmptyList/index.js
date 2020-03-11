d85290b2456ac759bc6311757ceb418a1ebc6f55
Sam Levy <slevy239@gmail.com>
2020-03-11 16:15:08 -0400

    strong

--- client/src/components/EmptyList/index.js
+++ client/src/components/EmptyList/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function EmptyList(){
     return(
         <div>
-            <h2>No Scores To Display</h2>
+            <h2>No More Games Today!</h2>
         </div>
     )
 }
