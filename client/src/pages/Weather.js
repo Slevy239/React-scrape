d1713314ee55f0708cfe5fdb8e2a4e479d6803c3
Sam Levy <slevy239@gmail.com>
2020-03-19 12:05:54 -0400

    weather api search

--- client/src/pages/Weather.js
+++ client/src/pages/Weather.js
@@ -14,49 +14,60 @@ class Weather extends Component {
         initialized: true
     }
 
-    componentDidMount() {
-        this.searchAPI();
-        this.getDb();
-    }
+    // componentDidMount() {
+    //     this.searchAPI();
+    //     // this.getDb();
+    // }
     displayRes = data => {
         this.setState({ results: data })
     }
-    getDb = () => {
-        axios.get("/api/results")
-          .then(res => {
-            this.setState({ db: res.data })
-          })
-          .catch((err => console.log(err)))
-      }
-
-
-searchAPI = () => {
-    const key = '3872ef922fdc7b4bc5f2921a05246757'
-    const url = 'http://api.openweathermap.org/data/2.5/weather?q=philadelphia&appid=' + key
-    axios.get(url)
-        .then(res => {
-            this.displayRes(res.data)
+    // getDb = () => {
+    //     axios.get("/api/results")
+    //         .then(res => {
+    //             this.setState({ db: res.data })
+    //         })
+    //         .catch((err => console.log(err)))
+    // }
+    handleInput = event => {
+        const { name, value } = event.target
+        this.setState({
+            [name]: value
         })
-        .catch(err => console.log(err))
-}
-render() {
-    console.log(this.state.results.visibility)
-    console.log(this.state.search)
-    return (
-        <div>
-            <Row>
-                <Grid>
-                    <form noValidate autoComplete='off'>
-                        <TextField />
-                    </form>
-                    <div>{this.state.results.name}</div>
-                    <div>{this.state.results.visibility}</div>
 
-                </Grid>
-            </Row>
+    }
+    searchAPI = () => {
+        const key = '3872ef922fdc7b4bc5f2921a05246757'
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.search}&appid=` + key
+        axios.get(url)
+            .then(res => {
+                this.displayRes(res.data)
+            })
+            .catch(err => console.log(err))
+    }
+    render() {
+        console.log(this.state.results.visibility)
+        console.log(this.state.search)
+        return (
+            <div>
+                <Row>
+                    <Grid>
+                        <form noValidate autoComplete='off'>
+                            <TextField
+                                name='search'
+                                onChange={this.handleInput}
+                            />
+                            <button type="submit" className="btn btn-danger" onClick={this.searchAPI} >
+                                Search for Books
+                             </button>
+                        </form>
+                        <div>{this.state.results.name}</div>
+                        <div>{this.state.results.visibility}</div>
 
-        </div>
-    )
-}
+                    </Grid>
+                </Row>
+
+            </div>
+        )
+    }
 }
 export default Weather
