8e5857c17e704bd870b3c4c4e0049986b27b2e1e
Sam Levy <slevy239@gmail.com>
2020-03-30 11:12:37 -0400

    update

--- client/src/pages/UFC.js
+++ client/src/pages/UFC.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import Button from '@material-ui/core/Button'
 
 class UFC extends Component {
     state = {
@@ -11,12 +12,13 @@ class UFC extends Component {
     }
 
     searchAPI = () => {
+        console.log("clicked")
         const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
         const url = proxyurl + 'https://api.sportradar.us/ufc/trial/v2/en/rankings.json?api_key=vu2y48dqrqwckk3jp2gu5pes'
         axios.get(url)
             .then(res => {
-                this.displayRes(res.data.rankings[0])
+                this.displayRes(res.data)
             })
             .catch(err => console.log(err))
     }
@@ -25,18 +27,15 @@ class UFC extends Component {
         console.log(this.state.results)
         return (
             <div>
-                <button onClick={this.searchAPI}>button</button>
+                <Button 
+                variant='contained'
+                color='primary'
+                onClick={this.searchAPI}>button</Button>
                 {(!this.state.results) ?
                     <div>nothing to show!</div>
                     :
                     <div>
-                        <div>{this.state.results.name.split('_')}</div>
-                        {/* {this.state.results.rankings.map(result => {
-                            return (
-                                <div>{result.year}</div>
-                            )
-
-                        })} */}
+                        <div>{this.state.results.generated_at}</div> 
                     </div>
                 }
             </div>
@@ -53,4 +52,4 @@ export default UFC
 
 
 
-const url = 'https://api.sportradar.us/ufc/trial/v2/en/rankings.json?api_key=vu2y48dqrqwckk3jp2gu5pes'
+// const url = 'https://api.sportradar.us/ufc/trial/v2/en/rankings.json?api_key=vu2y48dqrqwckk3jp2gu5pes'
