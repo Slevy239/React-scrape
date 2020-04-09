9c695db65ab2de3c069582802a46dccf5f8c0b5b
Sam Levy <slevy239@gmail.com>
2020-04-09 12:19:52 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,21 @@ import Tabs from './components/Tabs/Tabs'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../src/pages/Home'
 // import Tabs from '../src/components/Tabs'
-// import Drawer from './components/Drawer/Drawer'
+import Drawer from './components/Drawer/Drawer'
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/Rankings" component={Tabs} />
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      {/* <Drawer /> */}
+      <Router>
+        <div>
+          <Switch>
+            {/* <Route exact path="/" component={Home} /> */}
+            <Route exact path="/" component={Tabs} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
     // <div className="App">
     //   <Tabs />
     // </div>
